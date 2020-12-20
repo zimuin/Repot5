9015403da102b940e942b47e0a96745db8f77a72
zimuin <e205736@ie.u-ryukyu.ac.jp>
2020-12-20 16:56:23 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/ie/e205736/Main.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e205736;
+
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+
+    }
+}
