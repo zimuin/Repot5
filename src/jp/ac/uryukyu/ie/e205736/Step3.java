deb462b8256f5c91d381a1151d44abaaccfb1181
zimuin <e205736@ie.u-ryukyu.ac.jp>
2020-12-20 18:04:53 +0900

    3rdd commit

---
+++ src/jp/ac/uryukyu/ie/e205736/Step3.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e205736;
+
+public class Step3 {
+    public static void main(String[] args){
+        String str = "百二十三";
+        int value =  Integer.parseInt(str);
+    }
+    
+}
