25b7c6ef5bf61ab6b52a75d0d9d9d831fe0d5186
zimuin <e205736@ie.u-ryukyu.ac.jp>
2020-12-20 17:30:05 +0900

    2nd commit

--- src/jp/ac/uryukyu/ie/e205736/Main.java
+++ src/jp/ac/uryukyu/ie/e205736/Main.java
@@ -3,7 +3,11 @@ package jp.ac.uryukyu.ie.e205736;
 public class Main {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try{System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("エラー　NullPointException");
+            System.out.println(e.getMessage());
+        }
 
     }
 }
